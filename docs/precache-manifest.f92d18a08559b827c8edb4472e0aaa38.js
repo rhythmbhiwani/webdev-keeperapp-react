11b0ebb83fd7bea66fbdd5b25711ddd4a575a59a
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-08-14 18:31:29 +0530

    Hosted Files Added

---
+++ docs/precache-manifest.f92d18a08559b827c8edb4472e0aaa38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3f780805ec02062bc82ae2162d7c83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd158ab17a105aef108",
+    "url": "/static/js/2.fb6728fe.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.fb6728fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac317979f96af786a483",
+    "url": "/static/js/main.fbd0f1f2.chunk.js"
+  },
+  {
+    "revision": "272b2118ce1a3384d9bd",
+    "url": "/static/js/runtime-main.342c7561.js"
+  }
+]);
