3783481b22f2e645d015d979ab69b6d93d6c2ba2
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-08-14 18:34:19 +0530

    Host Files Update

---
+++ docs/precache-manifest.7d544602de93802b79640785b945bce3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd32d035cb498a1bf284b59cd87d6766",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5fd158ab17a105aef108",
+    "url": "./static/js/2.fb6728fe.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "./static/js/2.fb6728fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac317979f96af786a483",
+    "url": "./static/js/main.fbd0f1f2.chunk.js"
+  },
+  {
+    "revision": "a54a488295058869e082",
+    "url": "./static/js/runtime-main.24c86ffe.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f92d18a08559b827c8edb4472e0aaa38.js"
+  "./precache-manifest.7d544602de93802b79640785b945bce3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
